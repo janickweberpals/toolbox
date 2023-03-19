f06ae61e444aa5147027de2af4849b71a6b1de4a
Janick Weberpals <janick.weberpals@me.com>
2023-03-19 16:53:53 -0400

    some small modifications

--- R/format_table.R
+++ R/format_table.R
@@ -1,9 +1,10 @@
 #' Creates pretty formatted tables with few lines of code for both html and pdfs
 #'
 #' @param data dataframe, tibble or tableone object with table content
-#' @param format_out character, one of "html" or "latex" to choose from for optimized output
+#' @param col_bold should first column be printed bold
 #' @param font_size numeric, font size of table
 #' @param caption string, if table caption is desired
+#' @param ... more parameters passed on
 #'
 #' @return returns a pretty table formatted for html or latex
 #'
@@ -20,10 +21,13 @@
 #' @export
 #'
 format_table <- function(data = NULL, # tableone object or final df
-                         format_out = "html", # can be of type html or latex
-                         font_size = 16,
-                         caption = NULL
+                         col_bold = FALSE, # should first column be printed bold
+                         font_size = 15,
+                         caption = NULL,
+                         ...
                          ){
+
+  arguments <- list(...)
 
   # checks
   if("TableOne" %in% class(data)){
@@ -41,7 +45,7 @@ format_table <- function(data = NULL, # tableone object or final df
 
   }else(
 
-    stop("data is not a TableOne or data.frame object")
+    stop("data is not a TableOne or data.frame/tibble object")
 
   )
 
@@ -53,10 +57,10 @@ format_table <- function(data = NULL, # tableone object or final df
   return_table <- table_tmp %>%
 
     kableExtra::kable(
+      format = "html",
       booktabs = TRUE,
       linesep = "",
       longtable = TRUE,
-      format = format_out,
       caption = caption
       ) %>%
 
@@ -68,6 +72,11 @@ format_table <- function(data = NULL, # tableone object or final df
       html_font = "Minion"
       ) %>%
 
+    # kableExtra::kable_styling(
+    #   latex_options = c("repeat_header"),
+    #   repeat_header_continued = "\\textit{(Continued on next page...)}"
+    #   ) %>%
+
     # indentation of rows with categorical variables
     kableExtra::add_indent(
       vec_indent,
@@ -77,8 +86,14 @@ format_table <- function(data = NULL, # tableone object or final df
 
     # table header in bold font
     kableExtra::row_spec(0, bold = TRUE)
-  #kableExtra::row_spec(vec_bold, bold = TRUE) %>%
-  #kableExtra::column_spec(1:1, bold = TRUE)
+
+  if(isTRUE(return_table)){
+
+    # first column bold
+    return_table <- return_table %>%
+      kableExtra::column_spec(1:1, bold = TRUE)
+
+    }
 
   return(return_table)
 
